b7e4104fe2a2c931526acfeb77808ee52372adda
varsha-ramesh <119481468+varsha-ramesh@users.noreply.github.com>
2025-07-06 20:28:22 +0530

    Add files via upload

---
+++ dorm-learn/orm-learn/country_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE country (
+    co_code VARCHAR(2) PRIMARY KEY,
+    co_name VARCHAR(50)
+);
+
+INSERT INTO country VALUES ('IN', 'India');
+INSERT INTO country VALUES ('US', 'United States of America');
