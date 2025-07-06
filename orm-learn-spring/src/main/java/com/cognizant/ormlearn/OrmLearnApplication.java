b41bed64dd9126f79c04c6de7f1988f0f879d477
varsha-ramesh <119481468+varsha-ramesh@users.noreply.github.com>
2025-07-06 20:44:08 +0530

    Add files via upload

---
+++ orm-learn-spring/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,32 @@
+package com.cognizant.ormlearn;
+
+import com.cognizant.ormlearn.service.CountryService;
+import com.cognizant.ormlearn.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.List;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static CountryService countryService;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        countryService = context.getBean(CountryService.class);
+        testGetAllCountries();
+    }
+
+    private static void testGetAllCountries() {
+        LOGGER.info("Start");
+        List<Country> countries = countryService.getAllCountries();
+        LOGGER.debug("countries={}", countries);
+        LOGGER.info("End");
+    }
+}
